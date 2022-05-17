db8d93cf9c2b12dafdf296f03d2d61abcd90404a
vkchoudhary05 <102945131+vkchoudhary05@users.noreply.github.com>
2022-05-17 12:54:27 +0530

    FirstCommit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-Parser');
+const route = require('./routes/routes.js')
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://new-user_2020:Vivek2020@cluster0.lxwkw.mongodb.net/project03?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function (){
+    console.log('Express app running on port: ' + (process.env.PORT || 3000))
+});
+
+
+
